93759b837f8571fc510cff21e7e4cc155e49a394
Yaroslava Yemelianenko <71556255+YashaBlendeer@users.noreply.github.com>
2021-12-15 23:46:54 +0200

    added controller

---
+++ src/utils/Auth.js
@@ -0,0 +1,86 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const passport = require("passport");
+const User = require("../models/User");
+const { SECRET } = require("../config");
+
+const userRegister = async (userDets, role, res) => {
+  try {
+    // Validate the username
+    let usernameNotTaken = await validateUsername(userDets.username);
+    if (!usernameNotTaken) {
+      return res.status(400).json({
+        message: `Username is already taken.`,
+        success: false
+      });
+    }
+
+    // validate the email
+    let emailNotRegistered = await validateEmail(userDets.email);
+    if (!emailNotRegistered) {
+      return res.status(400).json({
+        message: `Email is already registered.`,
+        success: false
+      });
+    }
+
+    // Get the hashed password
+    const password = await bcrypt.hash(userDets.password, 12);
+    // create a new user
+    const newUser = new User({
+      ...userDets,
+      password,
+      role
+    });
+
+    await newUser.save();
+    return res.status(201).json({
+      message: "Hurry! now you are successfully registred. Please nor login.",
+      success: true
+    });
+  } catch (err) {
+    // Implement logger function (winston)
+    return res.status(500).json({
+      message: "Unable to create your account.",
+      success: false
+    });
+  }
+};
+
+
+
+const validateUsername = async username => {
+  let user = await User.findOne({ username });
+  return user ? false : true;
+};
+
+const userAuth = passport.authenticate("jwt", { session: false });
+
+const checkRole = roles => (req, res, next) =>
+  !roles.includes(req.user.role)
+    ? res.status(401).json("Unauthorized")
+    : next();
+
+const validateEmail = async email => {
+  let user = await User.findOne({ email });
+  return user ? false : true;
+};
+
+const serializeUser = user => {
+  return {
+    username: user.username,
+    email: user.email,
+    name: user.name,
+    _id: user._id,
+    updatedAt: user.updatedAt,
+    createdAt: user.createdAt
+  };
+};
+
+module.exports = {
+  userAuth,
+  checkRole,
+  userLogin,
+  userRegister,
+  serializeUser
+};
