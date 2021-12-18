4718eeeb844020b51d5a5cb5ff3c1c2c3039258a
Yaroslava Yemelianenko <71556255+YashaBlendeer@users.noreply.github.com>
2021-12-18 14:52:02 +0200

    update categories route

---
+++ routes/categories.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const {
+  getCategories,
+  getCategory,
+  createCategory,
+  updateCategory,
+  deleteCategory,
+  categoryPhotoUpload,
+} = require('../controllers/categories');
+
+const Category = require('../models/Category');
+
+const router = express.Router({ mergeParams: true });
+
+const advancedResults = require('../middleware/advancedResults');
+const { protect, authorize } = require('../middleware/auth');
+
+router.use(protect);
+// router.use()
+
+router
+  .route('/')
+  .get(advancedResults(Category, [{ path: 'questions' }]), getCategories)
+  .post(authorize('admin'), createCategory);
+
+router
+  .route('/:id')
+  .get(getCategory)
+  .put(authorize('admin'), updateCategory)
+  .delete(authorize('admin'), deleteCategory);
+
+router.route('/:id/photo').put(authorize('admin'), categoryPhotoUpload);
+
+module.exports = router;
